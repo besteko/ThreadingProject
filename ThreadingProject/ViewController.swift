6ee05a1021f98659e13fd7d3405c682f9d248b71
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-08-14 12:50:17 +0300

    commit

--- ThreadingProject/ViewController.swift
+++ ThreadingProject/ViewController.swift
@@ -7,11 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let urlStrings = ["https://www.noaa.gov/sites/default/files/styles/landscape_width_1275/public/legacy/image/2019/Jun/PHOTO%20-%20Genus%20Cyanea%2C%20a%20jellyfish%20well-known%20to%20bloom%20and%20occur%20in%20large%20numbers%20in%20surface%20waters%20-%20NOAA%20-%201125x534%20-%20Landscape.jpg", "https://upload.wikimedia.org/wikipedia/commons/7/74/Earth_poster_large.jpg"]
+    
+    var data = Data()
+    var tracker = 0
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        DispatchQueue.global().async {
+            self.data = try! Data(contentsOf: URL(string: self.urlStrings[self.tracker])!) //background thread
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: self.data) // main thread
+            }
+            
+        }
+        
+        
+        
+        
+        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(changeImage))
+        
+        }
+    
+    
+    @objc func changeImage() {
+        
+        if tracker == 0 {
+            tracker += 1
+        } else {
+            tracker -= 1
+        }
+        
+        
+        DispatchQueue.global().async {
+            self.data = try! Data(contentsOf: URL(string: self.urlStrings[self.tracker])!) //background thread
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: self.data) // main thread
+            }
+            
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Threading Test"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 25
     }
 
 
